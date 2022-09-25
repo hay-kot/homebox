17c29efdebf06e71f34b31a50c633ec8900116dc
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-09-24 18:36:18 -0800

    (WIP) unify card styles

---
+++ frontend/components/Base/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card bg-base-100 shadow-xl sm:rounded-lg">
+    <div class="px-4 py-5 sm:px-6">
+      <h3 class="text-lg font-medium leading-6">
+        <slot name="title"></slot>
+      </h3>
+      <p v-if="$slots.subtitle" class="mt-1 max-w-2xl text-sm text-gray-500">
+        <slot name="subtitle"></slot>
+      </p>
+      <template v-if="$slots['title-actions']">
+        <slot name="title-actions"></slot>
+      </template>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
