ba38b162c86fb1b8d8baaea39fccd9593de8857e
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-11-01 14:13:04 -0800

    resolve imports in testing

--- frontend/test/vitest.config.ts
+++ frontend/test/vitest.config.ts
@@ -1,7 +1,14 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
     globalSetup: "./test/setup.ts",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, ".."),
+      "~~": path.resolve(__dirname, ".."),
+    },
   },
 });
