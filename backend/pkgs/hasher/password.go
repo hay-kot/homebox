0e5d9528be386fff8b8fd47c77b20382e049c792
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-12-05 17:51:11 -0900

    spelling

--- backend/pkgs/hasher/password.go
+++ backend/pkgs/hasher/password.go
@@ -13,7 +13,7 @@ func init() {
 	disableHas := os.Getenv("UNSAFE_DISABLE_PASSWORD_PROJECTION") == "yes_i_am_sure"
 
 	if disableHas {
-		fmt.Println("WARNING: Password projection is disabled. This is unsafe in production.")
+		fmt.Println("WARNING: Password protection is disabled. This is unsafe in production.")
 		enabled = false
 	}
 }
