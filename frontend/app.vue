2e96d8c4c2e2f8512417bc105e271bac2dc86050
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-02-12 15:14:11 -0900

    fix favicon error

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLayout>
     <Html lang="en" :data-theme="theme || 'homebox'" />
+    <Link rel="icon" type="image/svg" href="/favicon.svg"></Link>
     <NuxtPage />
   </NuxtLayout>
 </template>
