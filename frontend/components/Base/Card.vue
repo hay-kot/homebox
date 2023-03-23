40e76bac0c9673e1009ffec1539bfff22edb22b3
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-03-22 19:05:13 -0800

    feat: filter details for zero values (#364)
    * filter details for zero values
    
    * ensure exhaustive checks
    
    * update event listener to only bind when collapsable

--- frontend/components/Base/Card.vue
+++ frontend/components/Base/Card.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="card bg-base-100 shadow-xl sm:rounded-lg">
     <div v-if="$slots.title" class="px-4 py-5 sm:px-6">
-      <h3 class="text-lg font-medium leading-6">
-        <slot name="title"></slot>
-      </h3>
-      <p v-if="$slots.subtitle" class="mt-1 max-w-2xl text-sm text-gray-500">
-        <slot name="subtitle"></slot>
-      </p>
-      <template v-if="$slots['title-actions']">
-        <slot name="title-actions"></slot>
-      </template>
+      <component :is="collapsable ? 'button' : 'div'" v-on="collapsable ? { click: toggle } : {}">
+        <h3 class="text-lg font-medium leading-6 flex items-center">
+          <slot name="title"></slot>
+          <template v-if="collapsable">
+            <span class="ml-2 swap swap-rotate" :class="`${collapsed ? 'swap-active' : ''}`">
+              <Icon class="h-6 w-6 swap-on" name="mdi-chevron-right" />
+              <Icon class="h-6 w-6 swap-off" name="mdi-chevron-down" />
+            </span>
+          </template>
+        </h3>
+      </component>
+      <div>
+        <p v-if="$slots.subtitle" class="mt-1 max-w-2xl text-sm text-gray-500">
+          <slot name="subtitle"></slot>
+        </p>
+        <template v-if="$slots['title-actions']">
+          <slot name="title-actions"></slot>
+        </template>
+      </div>
     </div>
-    <slot />
+    <div
+      :class="{
+        'max-h-screen': !collapsed,
+        'max-h-0': collapsed,
+      }"
+      class="transition-[max-height] duration-200 overflow-hidden"
+    >
+      <slot />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  defineProps<{
+    collapsable?: boolean;
+  }>();
+
+  function toggle() {
+    collapsed.value = !collapsed.value;
+  }
+
+  const collapsed = ref(false);
+</script>
--- frontend/components/global/DetailsSection/types.ts
+++ frontend/components/global/DetailsSection/types.ts
@@ -36,3 +36,23 @@ export type Detail = BaseDetail & {
 export type AnyDetail = DateDetail | CurrencyDetail | LinkDetail | MarkdownDetail | Detail;
 
 export type Details = Array<Detail | AnyDetail>;
+
+export function filterZeroValues(details: Details): Details {
+  return details.filter(detail => {
+    switch (detail.type) {
+      case "date":
+        return validDate(detail.text);
+      case "currency":
+        return !!detail.text;
+      case "link":
+        return !!detail.text && !!detail.href;
+      case undefined:
+      case "text":
+      case "markdown":
+        return detail.text !== null && detail.text !== "" && detail.text !== undefined;
+      default:
+        console.warn("Unknown detail type (this should never happen)", detail);
+        return false;
+    }
+  });
+}
