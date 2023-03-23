dd714573e8f93b23b374703cebfa7014737459ac
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-03-23 10:10:23 -0800

    add PWA support

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,6 +2,10 @@
   <NuxtLayout>
     <Html lang="en" :data-theme="theme || 'homebox'" />
     <Link rel="icon" type="image/svg" href="/favicon.svg"></Link>
+    <Link rel="apple-touch-icon" href="/apple-touch-icon.png" size="180x180" />
+    <Link rel="mask-icon" href="/mask-icon.svg" color="#5b7f67" />
+    <Meta name="theme-color" content="#5b7f67" />
+    <Link rel="manifest" href="/manifest.webmanifest" />
     <NuxtPage />
   </NuxtLayout>
 </template>
