bd6c24d35a21c54d6263af6ec55f3c13ae1fdb47
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-03-22 18:58:23 -0800

    ensure exhaustive checks

--- frontend/components/global/DetailsSection/types.ts
+++ frontend/components/global/DetailsSection/types.ts
@@ -46,8 +46,13 @@ export function filterZeroValues(details: Details): Details {
         return !!detail.text;
       case "link":
         return !!detail.text && !!detail.href;
-      default:
+      case undefined:
+      case "text":
+      case "markdown":
         return detail.text !== null && detail.text !== "" && detail.text !== undefined;
+      default:
+        console.warn("Unknown detail type (this should never happen)", detail);
+        return false;
     }
   });
 }
