5dd68445361ad916cbd3b8ede642292129189b65
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-03-23 19:10:19 -0800

    feat: change shit to things (#369)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -133,7 +133,7 @@
             <AppLogo class="w-12 -mb-4" />
             x
           </h2>
-          <p class="ml-1 text-lg text-base-content/50">Track, Organize, and Manage your Shit.</p>
+          <p class="ml-1 text-lg text-base-content/50">Track, Organize, and Manage your Things.</p>
         </div>
         <div class="flex mt-6 sm:mt-0 gap-4 ml-auto text-neutral-content">
           <a class="tooltip" data-tip="Project Github" href="https://github.com/hay-kot/homebox" target="_blank">
