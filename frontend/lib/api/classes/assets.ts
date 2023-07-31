ef9a7cd811fdfeee41d304cb8c65e0eca2e0e1be
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-07-31 06:05:37 -0800

    fix: assert/asserts (#521)
    Former-commit-id: 02ce52dbe358e4adbd11db1d0800cec037956b3a

--- frontend/lib/api/classes/assets.ts
+++ frontend/lib/api/classes/assets.ts
@@ -5,7 +5,7 @@ import { PaginationResult } from "../types/non-generated";
 export class AssetsApi extends BaseAPI {
   async get(id: string, page = 1, pageSize = 50) {
     return await this.http.get<PaginationResult<ItemSummary>>({
-      url: route(`/asset/${id}`, { page, pageSize }),
+      url: route(`/assets/${id}`, { page, pageSize }),
     });
   }
 }
